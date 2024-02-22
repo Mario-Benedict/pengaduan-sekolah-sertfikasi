398d85e309b981d32e465f888bb9b671e49732fc
Mario-Benedict <mariobenedict77@gmail.com>
2024-02-22 11:25:24 +0700

    fix: disable register route

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/profile', function () {
 
 Route::get('/search', [InputAspirasiController::class, 'search'])->name('search');
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
